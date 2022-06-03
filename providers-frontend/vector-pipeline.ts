1994af2b3a76693d032b38995a649b6432fccb3c
Frank Sandqvist <kankki@gmail.com>
2022-06-03 11:46:00 +0300

    ...

---
+++ providers-frontend/vector-pipeline.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+    frontendVectorPipelineVectorExpress
+} from './vector-express/vector-express-vector-pipeline';
+
+const frontendProvidersVectorPipeline: Record<string, FrontendVectorPipeline> =
+  {
+    VECTOR_EXPRESS: frontendVectorPipelineVectorExpress,
+  };
+
+export const vectorPipelineProvider = process.env.VECTOR_PIPELINE_PROVIDER
+  ? frontendProvidersVectorPipeline[process.env.VECTOR_PIPELINE_PROVIDER]
+  : frontendVectorPipelineVectorExpress;
+
+export interface FrontendVectorPipeline {
+  viewportComponent: React.FC<>;
+
+  selectionComponent: React.FC<>;
+}
